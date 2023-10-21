34a1bbba35025998c0445b5f716d36d17019ddf2
andrey_turchin <tur4intwo@gmail.com>
2023-10-21 15:00:06 +0300

    added express-handlebars and materialize css

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 const PORT = process.env.PORT || 5000;
+
 const app = express();
+
+const hbs = exphbs.create({
+	defaultLayout: 'main',
+	extname: 'hbs',
+})
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 app.use(cors());
 
 app.get('/', (req, res) => {
 	res.status(200);
-	res.sendFile(path.join(__dirname, 'views', 'index.html'));
+	res.render('index');
 });
 
 app.get('/about', (req, res) => {
 	res.status(200);
-	res.sendFile(path.join(__dirname, 'views', 'about.html'));
+	res.render('about');
 });
 
 
